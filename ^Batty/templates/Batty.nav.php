LoneFry/Batty

commit 2a7344bb27c4c340ee9dc8289c3aa474e5197a4d

Add search feature to Batty [Batty #0127]

^Batty/templates/Batty.nav.php
@@ -1,0 +1,180 @@
+<div class="Batty_section">
+	<div id="Batty_nav">
+		<a href="/Batty/home">Home</a>
+		<a href="/Batty/projects">Projects</a>
+		<a href="/Batty/users">Users</a>
+		<a href="/Batty/report">Report an Issue</a>
+	</div>
+		<form action="/Batty/search" method="GET">
+			<div class="Batty_searchContainer">
+				<div id="Batty_searchForm">
+					<input class="Batty_searchBox" type="text" name="search" value="<?php echo isset($search)?$search:''; ?>" />
+					<input type="submit" value="Search" />
+				</div>
+
+				<details class="Batty_details<?php echo isset($openFlag)?' open':''; ?>">
+					<summary>Additional Options</summary>
+					<div class="Batty_tableWrapper">
+						<table class="Batty_search">
+							<thead>
+								<tr>
+									<th>
+										<label title="Click to toggle all.">
+											<input type="checkbox" onchange="checkAllClick(this);" name="project_id_checkAll"<?php echo count($projectsUsed) == count($projects)?' checked="checked"':''; ?> />
+											Project:
+										</label>
+									</th>
+									<th>
+										<label title="Click to toggle all.">
+											<input type="checkbox" onchange="checkAllClick(this);" name="handler_id_checkAll"<?php echo count($handlersUsed) == count($users)?' checked="checked"':''; ?> />
+											Assignee:
+										</label>
+									</th>
+									<th>
+										<label title="Click to toggle all.">
+											<input type="checkbox" onchange="checkAllClick(this);" name="reporter_id_checkAll"<?php echo count($reportersUsed) == count($users)?' checked="checked"':''; ?> />
+											Reporter:
+										</label>
+									</th>
+									<th>
+										<label title="Click to toggle all.">
+											<input type="checkbox" onchange="checkAllClick(this);" name="status_checkAll"<?php echo count($statusesUsed) == count($statuses)?' checked="checked"':''; ?> />
+											Status:
+										</label>
+									</th>
+									<th>
+										<label title="Click to toggle all.">
+											<input type="checkbox" onchange="checkAllClick(this);" name="type_checkAll"<?php echo count($typesUsed) == count($types)?' checked="checked"':''; ?> />
+											Type:
+										</label>
+									</th>
+									<th>
+										<label title="Click to toggle all.">
+											<input type="checkbox" onchange="checkAllClick(this);" name="priority_checkAll"<?php echo count($prioritiesUsed) == count($priorities)?' checked="checked"':''; ?> />
+											Priority:
+										</label>
+									</th>
+								</tr>
+							</thead>
+							<tr>
+								<td>
+									<ul>
+										<?php
+										foreach($projects as $project_id => $project) {
+											if (in_array($project_id, $projectsUsed)) {
+												$checked = ' checked="checked"';
+											} else {
+												$checked = '';
+											}
+										?>
+										<li>
+											<label>
+												<input type="checkbox" name="project_id[]" class="project_id" onchange="updateCheckAll(this);" value="<?php echo $project_id; ?>"<?php echo $checked; ?> />
+												<?php echo $project->label; ?>
+											</label>
+										</li>
+									<?php } ?>
+									</ul>
+								</td>
+								<td>
+									<ul>
+										<?php
+										foreach($users as $login_id => $loginname) {
+											if (in_array($login_id, $handlersUsed)) {
+												$checked = ' checked="checked"';
+											} else {
+												$checked = '';
+											}
+										?>
+										<li>
+											<label>
+												<input type="checkbox" name="handler_id[]" class="handler_id" onchange="updateCheckAll(this);" value="<?php echo $login_id; ?>"<?php echo $checked; ?> />
+												<?php echo $loginname; ?>
+											</label>
+										</li>
+									<?php } ?>
+									</ul>
+								</td>
+								<td>
+									<ul>
+										<?php
+										foreach($users as $login_id => $loginname) {
+											if (in_array($login_id, $reportersUsed)) {
+												$checked = ' checked="checked"';
+											} else {
+												$checked = '';
+											}
+										?>
+										<li>
+											<label>
+												<input type="checkbox" name="reporter_id[]" class="reporter_id" onchange="updateCheckAll(this);" value="<?php echo $login_id; ?>"<?php echo $checked; ?> />
+												<?php echo $loginname; ?>
+											</label>
+										</li>
+									<?php } ?>
+									</ul>
+								</td>
+								<td>
+									<ul>
+										<?php
+										foreach($statuses as $statusLevel => $description) {
+											if (in_array($description, $statusesUsed)) {
+												$checked = ' checked="checked"';
+											} else {
+												$checked = '';
+											}
+										?>
+										<li>
+											<label>
+												<input type="checkbox" name="status[]" class="status" onchange="updateCheckAll(this);" value="<?php echo $description; ?>"<?php echo $checked; ?> />
+												<?php echo $description; ?>
+											</label>
+										</li>
+									<?php } ?>
+									</ul>
+								</td>
+								<td>
+									<ul>
+										<?php
+										foreach($types as $type) {
+											if (in_array($type, $typesUsed)) {
+												$checked = ' checked="checked"';
+											} else {
+												$checked = '';
+											}
+										?>
+										<li>
+											<label>
+												<input type="checkbox" name="type[]" class="type" onchange="updateCheckAll(this);" value="<?php echo $type; ?>"<?php echo $checked; ?> />
+												<?php echo $type; ?>
+											</label>
+										</li>
+									<?php } ?>
+									</ul>
+								</td>
+								<td>
+									<ul>
+										<?php
+										foreach($priorities as $priorityLevel => $description) {
+											if (in_array($priorityLevel, $prioritiesUsed)) {
+												$checked = ' checked="checked"';
+											} else {
+												$checked = '';
+											}
+										?>
+										<li>
+											<label>
+												<input type="checkbox" name="priority[]" class="priority" onchange="updateCheckAll(this);" value="<?php echo $priorityLevel; ?>"<?php echo $checked; ?> />
+												<?php echo $priorityLevel.': '.$description; ?>
+											</label>
+										</li>
+									<?php } ?>
+									</ul>
+								</td>
+							</tr>
+						</table>
+					</div>
+				</details>
+			</div>
+		</form>
+	</div>

^Batty/controllers/BattyController.php
@@ -41,9 +41,33 @@ class BattyController extends Controller {
 	public function __construct($argv) {
 		parent::__construct($argv);
 
+		//Includes Batty's css
 		G::$V->_style('/^Batty/css/Batty.css');
+
+		//Makes HTML 5 <details> compatable for every browser
+		G::$V->_style('/^Batty/css/details-shim.min.css');
+		G::$V->_script('/^Batty/js/details-shim.min.js');
+
+		//Include batty.js in template
+		G::$V->_script('/^Batty/js/batty.js');
+
 		G::$V->priorities = G::$G['Batty']['priorities'];
-		G::$V->projects = Project::all();
+		G::$V->projects   = Project::all();
+		G::$V->types      = Issue::getTypes();
+		G::$V->statuses   = Issue::getStatuses();
+
+		//Get every user who can use Batty
+		$Role = new Role(array('label' => 'Batty'));
+		$Role->fill();
+		G::$V->users = $Role->getMembers('loginname');
+
+		//Initializes items which are used in Batty Search
+		G::$V->statusesUsed   = array();
+		G::$V->typesUsed      = array();
+		G::$V->prioritiesUsed = array();
+		G::$V->projectsUsed   = array();
+		G::$V->reportersUsed  = array();
+		G::$V->handlersUsed   = array();
 	}
 
 	/**
@@ -69,6 +93,65 @@ class BattyController extends Controller {
 	}
 
 	/**
+	 * Search through Batty Issues
+	 *
+	 * @param array $argv URL arguments
+	 *
+	 * @return void
+	 */
+	public function do_search($argv) {
+		//Validate user
+		if (!G::$S->roleTest(self::$role)) {
+			return $this->do_403($argv);
+		}
+
+		G::$V->_title    = 'Batty : Search';
+		G::$V->_template = 'Batty.Search.php';
+
+		//Retrieves search results
+		if (isset($_GET['search'])) {
+			require_once dirname(__DIR__).'/reports/SearchReport.php';
+
+			//Set search value
+			G::$V->search = trim($_GET['search']);
+
+			//Create new SearchReport
+			$Report = new SearchReport();
+
+			//Makes sure search has a value set
+			if (G::$V->search) {
+				$Report->search = G::$V->search;
+			}
+
+			//Loops over each optional field
+			$fields = array(
+				'priority'    => 'prioritiesUsed',
+				'status'      => 'statusesUsed',
+				'type'        => 'typesUsed',
+				'project_id'  => 'projectsUsed',
+				'reporter_id' => 'reportersUsed',
+				'handler_id'  => 'handlersUsed',
+				);
+			foreach ($fields as $fieldName => $valsUsed) {
+				if (!isset($_GET[$fieldName.'_checkAll']) && isset($_GET[$fieldName])) {
+					//Sets the field for the report
+					$Report->{$fieldName} = $_GET[$fieldName];
+
+					//Sets which items the user used in their search
+					G::$V->{$valsUsed}    = $_GET[$fieldName];
+
+					//IF this is set, the <details> will be displayed
+					G::$V->openFlag       = 1;
+				}
+			}
+
+			//Retrieves results
+			$Report->load();
+			G::$V->results = $Report->toArray();
+		}
+	}
+
+	/**
 	 * List Batty users
 	 *
 	 * @param array $argv command arguments vector
@@ -79,9 +162,6 @@ class BattyController extends Controller {
 		if (!G::$S->roleTest(self::$role)) {
 			return $this->do_403($argv);
 		}
-		$Role = new Role(array('label' => 'Batty'));
-		$Role->fill();
-		G::$V->users     = $Role->getMembers('loginname');
 		G::$V->userStats = Issue::getUserStats(array_keys(G::$V->users));
 
 		G::$V->_title    = 'Batty : Users';
@@ -102,17 +182,15 @@ class BattyController extends Controller {
 		if (!isset($argv[1]) || !is_numeric($argv[1])) {
 			return $this->do_users($argv);
 		}
-		$Role = new Role(array('label' => 'Batty'));
-		$Role->fill();
-		$users = $Role->getMembers('loginname');
-		if (!isset($users[$argv[1]])) {
+
+		if (!isset(G::$V->users[$argv[1]])) {
 			G::msg('Requested user not a current Batty user.', 'error');
 
 			return $this->do_users($argv);
 		}
 
 		G::$V->login_id  = $user_id = $argv[1];
-		G::$V->loginname = $users[$argv[1]];
+		G::$V->loginname = G::$V->users[$argv[1]];
 
 		G::$V->_title    = 'Batty : User';
 		G::$V->_template = 'Batty.User.php';
@@ -139,13 +217,7 @@ class BattyController extends Controller {
 			return $this->do_report($argv);
 		}
 
-		//Include batty.js in template
-		G::$V->_script('/^Batty/js/batty.js');
-
 		$issue = Issue::byPK($argv[1]);
-		$Role  = new Role(array('label' => 'Batty'));
-		$Role->fill();
-		$users = $Role->getMembers('loginname');
 
 		//Check to see if an existing subscription exists
 		$subscr = new IssueSubscription(array('login_id' => G::$S->Login->login_id, 'issue_id' => $issue->issue_id));
@@ -205,7 +277,7 @@ class BattyController extends Controller {
 				G::msg('You must set a priority', 'error');
 			} elseif (!in_array($_POST['type'], Issue::getTypes())) {
 				G::msg('You must select a type', 'error');
-			} elseif (0 < $_POST['handler_id'] && !in_array($_POST['handler_id'], array_keys($users))) {
+			} elseif (0 < $_POST['handler_id'] && !in_array($_POST['handler_id'], array_keys(G::$V->users))) {
 				G::msg('Unknown handler selected', 'error');
 			} elseif (!$update->comment && !$diff) {
 				G::msg('No comment or changes detected.');
@@ -270,12 +342,9 @@ class BattyController extends Controller {
 		}
 
 		G::$V->subscr     = $subscr;
-		G::$V->users      = $users;
 		G::$V->issue      = $issue;
 		G::$V->reporter   = !G::$V->issue->reporter_id ? new Login() : Login::byPK(G::$V->issue->reporter_id);
 		G::$V->handler    = !G::$V->issue->handler_id ? new Login() : Login::byPK(G::$V->issue->handler_id);
-		G::$V->types      = Issue::getTypes();
-		G::$V->statuses   = Issue::getStatuses();
 		G::$V->updates    = Update::byIssue(G::$V->issue->issue_id);
 		G::$V->_title     = 'Batty : Issue';
 		G::$V->_template  = 'Batty.Issue.php';
@@ -294,10 +363,6 @@ class BattyController extends Controller {
 		}
 
 		$issue = new Issue(true);
-
-		$Role = new Role(array('label' => 'Batty'));
-		$Role->fill();
-		$users = $Role->getMembers('loginname');
 
 		//Initialize issue subscription
 		$subscr = new IssueSubscription(array('login_id' => G::$S->Login->login_id));
@@ -324,7 +389,7 @@ class BattyController extends Controller {
 				G::msg('You must set a priority', 'error');
 			} elseif (!in_array($_POST['type'], Issue::getTypes())) {
 				G::msg('You must select a type', 'error');
-			} elseif (0 < $_POST['handler_id'] && !in_array($_POST['handler_id'], array_keys($users))) {
+			} elseif (0 < $_POST['handler_id'] && !in_array($_POST['handler_id'], array_keys(G::$V->users))) {
 				G::msg('Unknown handler selected', 'error');
 			} else {
 				G::msg('Saving Issue');
@@ -357,8 +422,6 @@ class BattyController extends Controller {
 		}
 
 		G::$V->subscr     = $subscr;
-		G::$V->users      = $users;
-		G::$V->types      = Issue::getTypes();
 		G::$V->issue      = $issue;
 		G::$V->_title     = 'Batty : Report Issue';
 		G::$V->_template  = 'Batty.Report.php';
@@ -402,9 +465,6 @@ class BattyController extends Controller {
 		} else {
 			return $this->do_projects($argv);
 		}
-
-		//Include batty.js in template
-		G::$V->_script('/^Batty/js/batty.js');
 
 		if (G::$S->roleTest(self::$role.'/Admin')
 			&& isset($_POST['label'])

^Batty/css/Batty.css
@@ -17,6 +17,7 @@
 }
 
 .Batty_tableWrapper table td {
+	vertical-align: top;
 	border: 1px solid #ccc;
 	background-color: #fff;
 }
@@ -77,6 +78,8 @@ div#Batty_Subscriber_Status + div {
 }
 
 div.Batty_section h3 {
+	display: inline;
+	margin-right: 10px;
 	margin-bottom: 0;
 }
 
@@ -175,4 +178,40 @@ span.js-charsRemaining {
 }
 .Batty_new_update {
 	font-weight: bold;
+}
+.Batty_details summary:hover{
+	cursor: pointer;
+}
+.Batty_details summary {
+	display: inline;
+	padding-top: 10px;
+}
+.Batty_details {
+	margin-top: 18px;
+}
+.Batty_search ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+}
+.Batty_search ul li {
+	margin: 0px 0px 5px 0px;
+    padding: 0;
+}
+.Batty_search th {
+	text-align: center;
+}
+.Batty_searchContainer {
+	width: 950px;
+	font-size: 10pt;
+}
+.Batty_searchContainer .Batty_tableWrapper {
+	border-style: none;
+}
+#Batty_searchForm {
+	float: left;
+}
+#Batty_nav {
+	float: left;
+	width: 605px;
 }

^Batty/css/details-shim.min.css
@@ -1,0 +1,1 @@
+details,summary{display:block}details.closed>*{display:none}details.closed>summary{display:block}details.open>summary{display:block}details.closed>summary:before{display:inline-block;content:"\25b6";padding:0 .1em;margin-right:.4em;font-size:.9em}details.open>summary:before{display:inline-block;content:"\25bc";padding:0;margin-right:.35em}

^Batty/js/batty.js
@@ -45,3 +45,53 @@ function Batty_Subscribe_() {
 		Batty_Ajax_Message.innerHTML = "Subscription failed.";
 	}
 }
+
+/**
+ * Checks/Unchecks a list of checkboxes by classname
+ *
+ * @param object checkBox The check "All" checkbox object
+ *
+ * @return void
+ */
+function checkAllClick(checkBox) {
+   //The class name of the checkboxes we will check/uncheck
+   var className     = checkBox.name.replace('_checkAll', '');
+
+   //Grabs every checkbox of the current class
+   var boxes = document.getElementsByClassName(className);
+
+   for (var i = 0; i < boxes.length; i++) {
+		boxes[i].checked = checkBox.checked;
+   }
+}
+
+/**
+ * Checks/Unchecks a the "All" checkbox
+ *
+ * @param object checkBox The checkbox updated
+ *
+ * @return void
+ */
+function updateCheckAll(checkBox) {
+   //Indicates whether we should check or uncheck the "All" checkbox
+   var checkFlag = true;
+
+   if (checkBox.checked) {
+	   //Grabs every checkbox of the current class
+	   var boxes = document.getElementsByClassName(checkBox.className);
+
+	   //Loops over every checkbox of the current class
+	   for (var i = 0; i < boxes.length; i++) {
+		   //"All" is checked, check box
+		   if (!boxes[i].checked) {
+			   checkFlag = false;
+			   break;
+		   }
+	   }
+   //Unchecks the "All" checkbox
+   } else {
+	   checkFlag = false;
+   }
+   //Checks/Unchecks the "All" checkbox
+   document.getElementsByName(checkBox.className + "_checkAll")[0].checked = checkFlag;
+}

^Batty/reports/IssueReport.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Main Report for 'Batty' issue tracking system
- * File: /^Batty/reports/Issues.php
+ * File: /^Batty/reports/IssueReport.php
  *
  * PHP version 5.3
  *

^Batty/reports/SearchReport.php
@@ -1,0 +1,74 @@
+<?php
+/**
+ * Searches for Batty Issues
+ * File: /^Batty/reports/SearchReport.php
+ *
+ * PHP version 5.3
+ *
+ * @category LoneFry
+ * @package  Batty
+ * @author   LoneFry <dev@lonefry.com>
+ * @license  Creative Commons CC-NC-BY-SA
+ * @link     http://github.com/LoneFry/Batty
+ */
+
+require_once LIB.'/Report.php';
+require_once dirname(__DIR__).'/models/Issue.php';
+require_once dirname(__DIR__).'/models/Update.php';
+require_once SITE.CORE.'/models/Login.php';
+
+/**
+ * Class SearchReport
+ *
+ * @category LoneFry
+ * @package  Batty
+ * @author   LoneFry <dev@lonefry.com>
+ * @license  Creative Commons CC-NC-BY-SA
+ * @link     http://github.com/LoneFry/Batty
+ * @see      Report.php
+ */
+class SearchReport extends Report {
+	protected static $query = '';
+
+	protected static $vars = array(
+		'search'      => array('type' => 's', 'sql' => "(i.`description` LIKE '%%%1\$s%%' OR i.`label` LIKE '%%%1\$s%%' OR u.`comment` LIKE '%%%1\$s%%')"),
+		'priority'    => array('type' => 'a', 'values' => array(), 'sql' => "i.`priority` IN (%s)"),
+		'status'      => array('type' => 'a', 'values' => array(), 'sql' => "i.`status` IN (%s)"),
+		'project_id'  => array('type' => 'a', 'values' => array(), 'sql' => "i.`project_id` IN (%s)"),
+		'reporter_id' => array('type' => 'a', 'values' => array(), 'sql' => "i.`reporter_id`  IN (%s)"),
+		'handler_id'  => array('type' => 'a', 'values' => array(), 'sql' => "i.`handler_id`  IN (%s)"),
+		'type'        => array('type' => 'a', 'values' => array(), 'sql' => "i.`type`  IN (%s)"),
+	);
+
+	/**
+	 * Initializes static properties
+	 *
+	 * @return void
+	 */
+	public static function prime() {
+		self::$query = "SELECT i.`issue_id`, i.`project_id`, i.`label`, i.`description`, i.`type`, i.`priority`,"
+			." i.`status`, i.`reporter_id`, i.`handler_id`, i.`iCreateDate`, i.`iAssignDate`, i.`iClosedDate`,"
+			." i.`recordChanged`, l1.`loginname` as `reporter`, l2.`loginname` as `handler`"
+			." FROM `".Issue::getTable()."` i"
+			." LEFT JOIN `".Update::getTable()."` u ON u.`issue_id` = i.`issue_id`"
+			." LEFT JOIN `".Login::getTable()."` l1 ON i.`reporter_id` = l1.`login_id`"
+			." LEFT JOIN `".Login::getTable()."` l2 ON i.`handler_id` = l2.`login_id`"
+			." WHERE %s"
+			." GROUP BY i.`issue_id`"
+			;
+	}
+
+	/**
+	 * Add a padded issue number for each issue_id
+	 *
+	 * @return void
+	 */
+	public function onload() {
+		if (is_array($this->_data)) {
+			foreach ($this->_data as $k => $v) {
+				$this->_data[$k]['num'] = str_pad($v['issue_id'], 4, 0, STR_PAD_LEFT);
+			}
+		}
+	}
+}
+SearchReport::prime();

^Batty/templates/Batty.Home.php
@@ -3,13 +3,7 @@
 
 	<h2>Batty : Home</h2>
 
-	<div id="Batty_nav">
-		<a href="/Batty/home">Home</a>
-		<a href="/Batty/projects">Projects</a>
-		<a href="/Batty/users">Users</a>
-		<a href="/Batty/report">Report an Issue</a>
-	</div>
-
+	<?php include_once 'Batty.nav.php'; ?>
 	<div class="Batty_section Batty_issueList" id="Batty_byHandler">
 		<h3>Open Issues Assigned To You</h3>
 		<?php

commit 98fd8c70910d921ed2a05113c0472b17b61c33a6

Correct bug introduced in 2a7344b
Looping over loginnames in Batty.nav.php caused the loginname in Batty.User.php to always
show the name of the last loginname listed in Batty.nav.php

^Batty/templates/Batty.nav.php
@@ -79,7 +79,7 @@
 								<td>
 									<ul>
 										<?php
-										foreach($users as $login_id => $loginname) {
+										foreach($users as $login_id => $loginnam) {
 											if (in_array($login_id, $handlersUsed)) {
 												$checked = ' checked="checked"';
 											} else {
@@ -89,7 +89,7 @@
 										<li>
 											<label>
 												<input type="checkbox" name="handler_id[]" class="handler_id" onchange="updateCheckAll(this);" value="<?php echo $login_id; ?>"<?php echo $checked; ?> />
-												<?php echo $loginname; ?>
+												<?php echo $loginnam; ?>
 											</label>
 										</li>
 									<?php } ?>
@@ -98,7 +98,7 @@
 								<td>
 									<ul>
 										<?php
-										foreach($users as $login_id => $loginname) {
+										foreach($users as $login_id => $loginnam) {
 											if (in_array($login_id, $reportersUsed)) {
 												$checked = ' checked="checked"';
 											} else {
@@ -108,7 +108,7 @@
 										<li>
 											<label>
 												<input type="checkbox" name="reporter_id[]" class="reporter_id" onchange="updateCheckAll(this);" value="<?php echo $login_id; ?>"<?php echo $checked; ?> />
-												<?php echo $loginname; ?>
+												<?php echo $loginnam; ?>
 											</label>
 										</li>
 									<?php } ?>